b14925108415f93780e86f875ecd8a00056ed7e4
Alan Dann <alan.dann@shotgunsoftware.com>
2014-02-03 17:43:01 +0000

    #24251 - updated default implementation of load/store publish data to use Perforce attributes

--- hooks/load_publish_data.py
+++ hooks/load_publish_data.py
@@ -13,35 +13,95 @@ Hook that gets called to load publish data for a path submitted to Perforce
 """
 
 import sgtk
-import os, sys
+from tank_vendor import yaml
+
+import os
+import sys
+import tempfile
+import binascii
  
 class LoadPublishData(sgtk.Hook):
     
-    def execute(self, path, user, workspace, revision, **kwargs):
+    def execute(self, depot_path, user, workspace, revision, **kwargs):
         """
         Load the specified publish data so that was previously stored by
         the corresponding save_publish_data hook
         
-        :publish_path:  String
-                        Path to the file being published
+        :depot_path:    String
+                        Depot path to the file being published
                         
         :user:          Dictionary
                         Shotgun HumanUser entity dictionary
                         
         :workspace:     String
                         The Perforce workspace/client that path is being published in
-                        
+                     
         :revision:      Int
-                        Revision of the published file
+                        Revision of the file
                      
         :returns:       Dictionary
                         Dictionary of data loaded for the published file.  This data should match 
                         the parameters expected by the 'sgtk.util.register_publish()' function.
         """
-        fw = self.parent
+        # the default implementation looks for the publish data in a p4 attribute 
+        # that lives with the file:
+        #
+        #    shotgun_metadata - contains a yaml version of all metadata
+        data = {}
         
-        # the default implementation does nothing!
-        return {}
+        p4_fw = self.parent
+        p4_util = p4_fw.import_module("util")
+        from P4 import P4Exception
+
+        # we'll need a Perforce connection:
+        p4 = p4_fw.connect()
+
+        sg_metadata = {}
+        try:    
+            p4_res = p4.run_fstat('-Oa', "%s#%d" % (depot_path, revision))
+            if p4_res:
+                sg_metadata_str = p4_res[0].get("attr-shotgun_metadata")
+                if sg_metadata_str:
+                    sg_metadata = yaml.load(sg_metadata_str)
+        except P4Exception, e:
+            return {}
+
+        if not sg_metadata:
+            return {}
+
+        # build data dictionary to return:
+        data["comment"] = sg_metadata.get("comment", "")
+        data["dependency_paths"] = sg_metadata.get("dependency_paths", [])
         
-        
-        
+        task = sg_metadata.get("task")
+        if task:
+            data["task"] = task
+
+        entity = sg_metadata.get("entity")
+        if entity:
+            ctx = p4_fw.sgtk.context_from_entity(entity["type"], entity["id"])
+            data["context"] = ctx
+
+        data["published_file_type"] = sg_metadata.get("published_file_type")
+            
+        # thumbnail:
+        if "thumbnail" in sg_metadata:
+            
+            thumbnail_str = sg_metadata["thumbnail"]
+            content = binascii.unhexlify(thumbnail_str)
+            thumbnail_sfx = sg_metadata.get("thumbnail_type", ".png")
+            
+            # have a thumbnail so save it to a temporary file:
+            temp_file, thumbnail_path = tempfile.mkstemp(suffix=thumbnail_sfx, prefix="tanktmp")
+            if temp_file:
+                os.close(temp_file)
+                
+            f = open(thumbnail_path, "wb")
+            try:
+                f.write(content)
+            finally:
+                f.close()
+                
+            data["thumbnail_path"] = thumbnail_path
+            
+        return data
--- hooks/store_publish_data.py
+++ hooks/store_publish_data.py
@@ -13,7 +13,12 @@ Hook that gets called to store publish data for a path being submitted to Perfor
 """
 
 import sgtk
-import os, sys
+from tank_vendor import yaml
+
+import os
+import sys
+import tempfile
+import binascii
  
 class StorePublishData(sgtk.Hook):
     
@@ -35,8 +40,77 @@ class StorePublishData(sgtk.Hook):
                         Dictionary of data to store for the published file.  This data will match the
                         parameters expected by the 'sgtk.util.register_publish()' function.
         """
-        fw = self.parent
         
-        # the default implementation does nothing!
-        pass
+        # the default implementation stores the publish data in a p4 attribute so 
+        # that it lives with the file:
+        #
+        #    shotgun_metadata - store a yaml version of all metadata
+        sg_metadata = {}
+                        
+        p4_fw = self.parent
+        p4_util = p4_fw.import_module("util")
+        from P4 import P4Exception
+
+        # we'll need a Perforce connection:
+        p4 = p4_fw.connect()
         
+        # dependencies:
+        dependency_paths = data.get("dependency_paths", [])
+        p4_file_details = p4_util.get_client_file_details(p4, dependency_paths)
+        depot_dependency_paths = []
+        for dp in dependency_paths:
+            ddp = p4_file_details[dp].get("depotFile")
+            if ddp:
+                # only care about dependency paths that have valid depot paths
+                # (TODO) - maybe this should error?
+                depot_dependency_paths.append(ddp)
+
+        sg_metadata["dependency_paths"] = depot_dependency_paths
+        
+        # entity & task:
+        task = data.get("task")
+        ctx = data.get("context")
+        if ctx:
+            if ctx.entity:
+                sg_metadata["entity"] = ctx.entity
+            if not task:
+                task = ctx.task
+        if task:
+            sg_metadata["task"] = task        
+        
+        # comment/description:
+        sg_metadata["comment"] = data.get("comment", "")
+        
+        # published file type:
+        sg_metadata["published_file_type"] = data.get("published_file_type", "")
+        
+        # thumbnail:
+        thumbnail_path = data.get("thumbnail_path")
+        if thumbnail_path and os.path.exists(thumbnail_path):
+            f = None
+            try:
+                f = open(thumbnail_path, "rb")
+                content = f.read()
+                _, sg_metadata["thumbnail_type"] = os.path.splitext(thumbnail_path)
+                sg_metadata["thumbnail"] = binascii.hexlify(content)
+            finally:
+                if f:
+                    f.close()
+                
+        # now format as yaml data:
+        sg_metadata_str = yaml.dump(sg_metadata)
+        
+        #print "sg_metadata_str"
+        #print sg_metadata_str
+        
+        # and finally, set it on the file in Perforce:
+        try:
+            # use '-p' to create a propogating attribute that will propogate with the file 
+            # when the file is opened for add, edit or delete.  This will ensure subsequent
+            # changes to the file retain this information unless it's modified by a future
+            # publish
+            p4.run_attribute("-p", "-n", "shotgun_metadata", "-v", sg_metadata_str, local_path)
+        except P4Exception, e:
+            raise TankError("Failed to store publish data in Perforce attribute for file '%s'" % local_path)
+
+
